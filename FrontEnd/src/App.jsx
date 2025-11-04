3d986bd18b34713ef974a0a83f0db68c05d3a1b1
ADI-2707 <aditya21singh2707@gmail.com>
2025-11-04 17:24:12 +0530

    Login/Sign Up form UI done

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,42 +1,41 @@
-
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar'
-import { useState } from 'react'
-import Home from './pages/Home.jsx'
-import Cars from './pages/Cars.jsx'
-import CarDetails from './pages/CarDetails.jsx'
-import MyBookings from './pages/MyBookings.jsx'
-import Footer from './components/Footer.jsx';
-import Layout from './pages/owner/Layout.jsx';
-import Dashboard from './pages/owner/Dashboard.jsx';
-import AddCar from './pages/owner/AddCar.jsx';
-import ManageCars from './pages/owner/ManageCars.jsx';
-import ManageBookings from './pages/owner/ManageBookings.jsx';
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { useState } from "react";
+import Home from "./pages/Home.jsx";
+import Cars from "./pages/Cars.jsx";
+import CarDetails from "./pages/CarDetails.jsx";
+import MyBookings from "./pages/MyBookings.jsx";
+import Footer from "./components/Footer.jsx";
+import Layout from "./pages/owner/Layout.jsx";
+import Dashboard from "./pages/owner/Dashboard.jsx";
+import AddCar from "./pages/owner/AddCar.jsx";
+import ManageCars from "./pages/owner/ManageCars.jsx";
+import ManageBookings from "./pages/owner/ManageBookings.jsx";
+import Login from "./components/Login.jsx";
 
 const App = () => {
-
   const [showLogin, setShowLogin] = useState(false);
-  const isOwnerPath = useLocation().pathname.startsWith('/owner');
+  const isOwnerPath = useLocation().pathname.startsWith("/owner");
 
   return (
     <>
+      {showLogin && <Login setShowLogin={setShowLogin} />}
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
-
       <Routes>
-        <Route path ='/' element ={<Home />} />
-        <Route path ='/cars' element ={<Cars />} />
-        <Route path ='/car-details/:id' element ={<CarDetails />} />
-        <Route path ='/my-bookings' element ={<MyBookings />} />
-        <Route path='/owner' element={<Layout />}>
-        <Route index element={<Dashboard />} />
-        <Route path='add-car' element={<AddCar />} />
-        <Route path='manage-cars' element={<ManageCars />} />
-        <Route path='manage-bookings' element={<ManageBookings />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/owner" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-car" element={<AddCar />} />
+          <Route path="manage-cars" element={<ManageCars />} />
+          <Route path="manage-bookings" element={<ManageBookings />} />
         </Route>
       </Routes>
       {!isOwnerPath && <Footer />}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
